3739bc397ae1b9f9c1f7d021493276a635081395
simon <si.sibomana@gmail.com>
2024-09-09 18:01:17 +0200

    parse header section

--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
 import socket
 import sys
 import logging
+from dataclasses import dataclass
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
 logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class Message:
+    header: bytes = b''
+    question: bytes = b''
+    answer: bytes = b''
+    authority: bytes = b''
+    additional: bytes = b''
+
+    def message(self):
+        return self.header + self.question + self.answer + self.authority + self.additional
+
+    def __repr__(self):
+        return f"{self.header + self.question + self.answer + self.authority + self.additional}"
+
 
 def main():
     logger.info("STARTING SERVER...")
@@ -12,61 +29,76 @@ def main():
     udp_socket: socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind((server_ip, server_port))
 
-    dns_message = {'HEADER':'', 'QUESTION':'', 'ANSWER':'', 'AUTHORITY':'', 'ADDITIONAL':''}
-
-    while True:
-        try:
+    try:
+        while True:
             buf, source = udp_socket.recvfrom(512)
             logger.info(f"received: {buf} from: {source}")
 
-            # Header
-            packet_id = bin(1234)[2:]
-            query_response_indicator = '1'
-            operation_code = '0000'
-            authoritative_answer = '0'
-            truncation = '0'
-            recursion_desired = '0'
-            recursion_available = '0'
-            reserved = '000'
-            response_code = '0000'
-            question_count = '0'*15 + '1'
-            answer_record_count = '0'*15 + '1'
-            authority_record_count = '0'*16
-            additional_record_count = '0'*16
+            response: bytes = generate_response(buf)
+            logger.info(f"response: {response} | response_length: {len(response)}")
 
-            # Question
-            question_name = '\x0ccodecrafters\x02io\x00'.encode()
-            question_type = int('0x0001', 0).to_bytes(2, 'big')
-            question_class = int('0x0001', 0).to_bytes(2, 'big')
-
-            # Answer
-            answer_ttl = int(60).to_bytes(4, 'big')
-            answer_length = int(4).to_bytes(2, 'big')
-            answer_data = b''.join(int(x, 0).to_bytes(1, 'big') for x in "8.8.8.8".split("."))
-
-
-            dns_message['HEADER'] = int((packet_id + query_response_indicator + operation_code + authoritative_answer
-                                     + truncation + recursion_desired + recursion_available + reserved
-                                     + response_code + question_count + answer_record_count + authority_record_count
-                                     + additional_record_count), 2).to_bytes(length=12, byteorder='big')
-
-            dns_message['QUESTION'] = (question_name + question_type + question_class)
-
-            dns_message['ANSWER'] = (dns_message['QUESTION'] + answer_ttl + answer_length + answer_data)
-
-            logger.info(f"response_header: {dns_message['HEADER']} | length: {len(dns_message['HEADER'])}")
-            logger.info(f"response_question: {dns_message['QUESTION']} | length: {len(dns_message['QUESTION'])}")
-            logger.info(f"response_answer: {dns_message['ANSWER']} | length: {len(dns_message['ANSWER'])}")
-
-            response = (dns_message['HEADER'] + dns_message['QUESTION'] + dns_message['ANSWER'])
-            print(f"response: {response} | response_length: {len(response)}")
             udp_socket.sendto(response, source)
-        except Exception as e:
-            logger.exception(f"Error receiving data: {e}")
-            break
-        except KeyboardInterrupt:
-            logger.exception("TERMINAL ERROR:")
-            break
+
+    except Exception as e:
+        logger.exception(f"Error receiving data: {e}")
+    except KeyboardInterrupt:
+        logger.exception("TERMINAL ERROR:")
+    finally:
+        logger.info("SERVER SHUT DOWN")
+
+
+def generate_response(buf: bytes) -> bytes:
+    dns_header = generate_response_header(buf)
+    dns_question = generate_response_question()
+    dns_answer = generate_response_answer(dns_question)
+
+    dns_message = Message(dns_header, dns_question, dns_answer)
+    return dns_message.message()
+
+
+def generate_response_header(buf: bytes) -> bytes:
+    header_size = 96
+    query_response_indicator = '1'
+    authoritative_answer = '0'
+    truncation = '0'
+    recursion_available = '0'
+    reserved = '000'
+    question_count = '0' * 15 + '1'
+    answer_record_count = '0' * 15 + '1'
+    authority_record_count = '0' * 16
+    additional_record_count = '0' * 16
+
+    dns_header = bin(int().from_bytes(buf[:12], 'big'))[2:]
+    if len(dns_header) < header_size:
+        dns_header = '0' * (header_size - len(dns_header)) + dns_header
+
+    packet_id = dns_header[:16]
+    operation_code = dns_header[17:21]
+    recursion_desired = dns_header[23:24]
+    response_code = '0000' if operation_code == '0000' else '0100'
+
+    dns_header = int((packet_id + query_response_indicator + operation_code + authoritative_answer
+                      + truncation + recursion_desired + recursion_available + reserved
+                      + response_code + question_count + answer_record_count + authority_record_count
+                      + additional_record_count), 2).to_bytes(length=12, byteorder='big')
+
+    return dns_header
+
+
+def generate_response_question() -> bytes:
+    question_name = '\x0ccodecrafters\x02io\x00'.encode()
+    question_type = int('0x0001', 0).to_bytes(2, 'big')
+    question_class = int('0x0001', 0).to_bytes(2, 'big')
+
+    return question_name + question_type + question_class
+
+
+def generate_response_answer(dns_question: bytes) -> bytes:
+    answer_ttl = int(60).to_bytes(4, 'big')
+    answer_length = int(4).to_bytes(2, 'big')
+    answer_data = b''.join(int(x, 0).to_bytes(1, 'big') for x in "8.8.8.8".split("."))
+
+    return dns_question + answer_ttl + answer_length + answer_data
 
 
 if __name__ == "__main__":
