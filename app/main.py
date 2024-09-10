35855663fb9b409ee205c270418ba28e2e42ad4b
simon <si.sibomana@gmail.com>
2024-09-10 18:28:26 +0200

    parse compressed packet

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import socket
 import sys
 import logging
-from dataclasses import dataclass
+from typing import List
+from dataclasses import dataclass, field
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
 logger = logging.getLogger(__name__)
@@ -10,16 +11,16 @@ logger = logging.getLogger(__name__)
 @dataclass(frozen=True)
 class Message:
     header: bytes = b''
-    question: bytes = b''
-    answer: bytes = b''
+    question: List[bytes] = field(default_factory=list)
+    answer: List[bytes] = field(default_factory=list)
     authority: bytes = b''
     additional: bytes = b''
 
     def message(self):
-        return self.header + self.question + self.answer + self.authority + self.additional
+        return self.header + b''.join(self.question) + b''.join(self.answer) + self.authority + self.additional
 
     def __repr__(self):
-        return f"{self.header + self.question + self.answer + self.authority + self.additional}"
+        return f"{self.header + b''.join(self.question) + b''.join(self.answer) + self.authority + self.additional}"
 
 
 def main():
@@ -48,11 +49,28 @@ def main():
 
 
 def generate_response(buf: bytes) -> bytes:
-    dns_header = generate_response_header(buf)
-    dns_question = generate_response_question(buf)
-    dns_answer = generate_response_answer(dns_question)
+    dns_header: bytes = generate_response_header(buf)
+    logger.info(f"dns_header: {dns_header}")
 
-    dns_message = Message(dns_header, dns_question, dns_answer)
+    question_count = int.from_bytes(dns_header[5:7], 'big')
+    logger.info(f"question_count: {question_count}")
+
+    dns_questions: List[bytes] = []
+    dns_answers: List[bytes] = []
+    question_index = 12
+
+    for _ in range(question_count):
+        dns_question = generate_response_question(buf, question_index)
+        dns_questions.append(dns_question)
+        question_index += len(dns_question)
+        logger.info(f"dns_question: {dns_question}")
+
+    for dns_question in dns_questions:
+        dns_answer = generate_response_answer(dns_question)
+        dns_answers.append(dns_answer)
+        logger.info(f"dns_answer: {dns_answer}")
+
+    dns_message = Message(dns_header, dns_questions, dns_answers)
     return dns_message.message()
 
 
@@ -63,8 +81,6 @@ def generate_response_header(buf: bytes) -> bytes:
     truncation = '0'
     recursion_available = '0'
     reserved = '000'
-    question_count = '0' * 15 + '1'
-    answer_record_count = '0' * 15 + '1'
     authority_record_count = '0' * 16
     additional_record_count = '0' * 16
 
@@ -75,6 +91,8 @@ def generate_response_header(buf: bytes) -> bytes:
     packet_id = dns_header[:16]
     operation_code = dns_header[17:21]
     recursion_desired = dns_header[23:24]
+    question_count = dns_header[32:48]
+    answer_record_count = question_count
     response_code = '0000' if operation_code == '0000' else '0100'
 
     dns_header = int((packet_id + query_response_indicator + operation_code + authoritative_answer
@@ -85,12 +103,27 @@ def generate_response_header(buf: bytes) -> bytes:
     return dns_header
 
 
-def generate_response_question(buf) -> bytes:
-    buf_index = 12
-    for buf_index in range(12, len(buf)):
-        if buf[buf_index: buf_index + 1] == b'\x00':
-            break
-    question_name = buf[12: buf_index + 1]
+def generate_response_question(buf: bytes, question_index) -> bytes:
+    label_prefix = bin(int().from_bytes(buf[question_index: question_index + 1], 'big'))[2:4]
+    logger.info(f"label_prefix: {label_prefix}")
+
+    if label_prefix == '11':
+        pointer_offset = bin(int().from_bytes(buf[question_index: question_index + 2], 'big'))[4:]
+        buf_offset = int(pointer_offset, 2)
+        buf_index = buf_offset
+        for buf_index in range(buf_offset, len(buf)):
+            if buf[buf_index: buf_index + 1] == b'\x00':
+                break
+        question_name = buf[buf_offset: buf_index + 1]
+        logger.info(f"question_name: {question_name}")
+    else:
+        buf_index = question_index
+        for buf_index in range(question_index, len(buf)):
+            if buf[buf_index: buf_index + 1] == b'\x00':
+                break
+        question_name = buf[question_index: buf_index + 1]
+        logger.info(f"question_name: {question_name}")
+
     question_type = int('0x0001', 0).to_bytes(2, 'big')
     question_class = int('0x0001', 0).to_bytes(2, 'big')
 
